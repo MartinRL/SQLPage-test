c1c32173fdc98eb52717e106c45bb13e9607d0f5
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-03-16 12:29:39 +0100

    list users

--- index.sql
+++ index.sql
@@ -5,3 +5,6 @@ SELECT 'Username' as name, TRUE as required;
 INSERT INTO users (name)
 SELECT :Username
 WHERE :Username IS NOT NULL;
+
+SELECT 'list' AS component, 'Users' AS title;
+SELECT name AS title,  name || ' is a user on this website.' as description FROM users;
